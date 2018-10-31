cb4c23f3de3f21663c7a4e31266bcc8c98e7c103
YI ZHANG <scaccterran@gmail.com>
2018-10-31 02:54:36 -0400

    removed database file from application since it will be acquired from external module

--- database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/snooker_data', { useNewUrlParser: true });
-
-module.exports = mongoose.connection;
