84d8e21ecf4bda9345294ded80ea726082a33c97
YI ZHANG <scaccterran@gmail.com>
2018-11-07 15:49:47 -0500

    check if heroku properly updates code base

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 global.config = require('config');
 
+console.log('updated');
+
 const connection = require('mongoose_model').connection;
 const PlayerCollector = require('./collectors/playerCollector');
 
